b9cb1c7d34b38595d1dd467c68219c633e385a99
Katharina Paul <katharina.paul@rwth-aachen.de>
2025-04-10 13:22:01 +0200

    change u from 0.5 to 0.3, create plot with 6 figures and expand explanaition texts

--- src/GRM_for_linear_LC.py
+++ src/GRM_for_linear_LC.py
@@ -15,7 +15,7 @@
 
 # %% [markdown]
 # (GRM_for_linear_LC)=
-# # Effects of dispersion on the elution of a rectangular pulse 
+# # Effects of the Peclet number on the elution of a rectangular pulse 
 
 # %% [markdown]
 # The following example is a reproduction of part of the research results published in "Analytical solutions and moment analysis of general rate model for linear liquid chromatography" (Shamsul Qamar, Javeria Nawaz Abbasi, Shumaila Javeed, Andreas Seidel-Morgenstern, Chemical Engineering Science 2014;107:192-205. doi:10.1016/j.ces.2013.12.019.). [**url**: here](https://doi.org/10.1016/j.ces.2013.12.019) <br> Their study of the General Rate Model for linear liquid chromatography was later used as a model problem by Leweke et al. for determining convergence benchmarks in CADET (_Chromatography Analysis and Design Toolkit (CADET)_, Samuel Leweke, Eric von Lieres, Computers & Chemical Engineering 2018;113:274-294. doi:10.1016/j.compchemeng.2018.02.025.)  [**url**: here](https://doi.org/10.1016/j.compchemeng.2018.02.025)
@@ -23,8 +23,9 @@
 #
 
 # %% [markdown]
-# In the example, a tracer is introduced into the column as a rectangular pulse. The binding behavior follows the Linear binding model. The unit operation model for this process is the General Rate Model.
-# All numerical values are taken from Table 2 (Leweke et al.)
+# In the given example, a tracer is introduced into the column as a rectangular pulse. The binding behavior follows the Linear binding model. The unit operation model for this process is the General Rate Model. In their study, Qamar at al. examined the influence of dispersion on the elution behavior by analysing the effects of different Peclet numbers (Fig.5). The Peclet number `Pe` is a value discribing the ratio of convection to dispersion for particle flow in a column. 
+#
+# In the following example, the elution is simulated for the numerical values from Table 2 (Leweke et al.). The interstitial velocity used here is u = 0.3 cm / min. Following this, the effects of different Peclet numbers are examined by simulating elutions for six different values.  
 # The `flow_rate` can be calculated as the product of the interstitial cross section area and the interstitial velocity u. 
 
 # %%
@@ -58,12 +59,12 @@ column.particle_porosity = 0.333  # ε_p [-]
 column.axial_dispersion = 3.33e-9  # D_c [m² / s]
 column.film_diffusion = column.n_comp * [1.67e-6]  # k_f [m / s]
 column.pore_diffusion = column.n_comp * [3.003e-6]  # D_p [m² / s]
-column.surface_diffusion = column.n_bound_states * [0.0]  #D_s [m² / s]
+column.surface_diffusion = column.n_bound_states * [0.0]  # D_s [m² / s]
 column.c = [0.0]  # [mM] 
 column.q = [0.0]  # [mM]  
 
 inlet = Inlet(component_system, name='inlet')
-inlet.flow_rate = column.cross_section_area_interstitial * (0.5 * (1e-2 / 60))  # m² * [m / s] 
+inlet.flow_rate = column.cross_section_area_interstitial * (0.3 * (1e-2 / 60))  # m² * [m / s] 
 
 outlet = Outlet(component_system, name='outlet')
 
@@ -85,7 +86,7 @@ process = Process(flow_sheet, 'pulse')
 process.cycle_time = 100 * 60  # [s]
 pulse_duration = 20.0 * 60  # [s]
 
-c_pulse = np.array([1.0])  #injection conc = 1.0 [mol / m³]
+c_pulse = np.array([1.0])  # injection concentration = 1.0 [mol / m³]
 c_initial = np.array([0.0])
   
 process.add_event('pulse_start', 'flow_sheet.inlet.c', c_pulse)
@@ -102,34 +103,31 @@ if __name__ == '__main__':
     simulation_results.solution.column.outlet.plot()
 
 # %% [markdown]
-# The Peclet number `Pe` is a value discribing the ratio of convection to dispersion for particle flow in a column. It is given by the product of the column length and the interstiatial velocity devided by the dispersion. The `peclet_number` of the column used by Leweke et al. and shown in the plot above is around 425.4. 
+# The Peclet number is given by the product of the column length and the interstiatial velocity devided by the dispersion. The `peclet_number` of the column used by Qamar et al. and shown in the plot above is around 255. 
 
 # %%
-peclet_number = (column.length * (0.5 * (1e-2 / 60))) / column.axial_dispersion
+peclet_number = (column.length * (0.3 * (1e-2 / 60))) / column.axial_dispersion
 peclet_number
 
 # %% [markdown]
-# The effects of different Peclet numbers on the elution curve can be examined by varying the dispersion rates within the column. The results show an approaching of the elution curve to a rectangular output as the `dispersion_value` is reduced and the Peclet number increases. The greater influence of convection/advective transport (directed flow of particles with the mobile phase (eluent)) in high Peclet numbers results in a sharper rectangular peak at the outlet as the velocities of the particles within the mobile phase differ less. The elution approaches the behavior of an ideal plug flow. Smaller Peclet numbers result in a more defined, slim peak and a more gradiual elution with larger retention times as the differences between particle velocities in the mobile phase increase. 
+# The effects of different Peclet numbers on the elution curve can be examined by varying the dispersion rates within the column. The results show an approaching of the elution profile to a rectangular output as the dispersion is reduced and the Peclet number increases. The greater influence of advective transport in high Peclet numbers results in an elution that is more similar to the concentration profile at the inlet. Throughout the column the velocities of the particles within the mobile phase differ less with increasing Peclet numbers. The elution approaches the behavior of an ideal plug flow reactor. 
+# Smaller Peclet numbers indicate a greater influence of dispersion. This results in a more gradual elution with larger retention times as the differences between particle velocities in the mobile phase increase. The resulting elution profile approaches the behavior of a CSTR.
 #
 
 # %%
-print(__name__)
 if __name__ == '__main__':
-    import CADETProcess
+    import matplotlib.pyplot as plt
     from CADETProcess.simulator import Cadet
     process_simulator = Cadet()
-
-    dispersion_value = [1.4166666666666667e-06,  # Pe = 1 
-                        2.8333333333333336e-07,  # Pe = 5
-                        1.4166666666666668e-07,  # Pe = 10
-                        5.666666666666667e-08,  # Pe = 25
-                        2.8333333333333336e-08,  # Pe = 50
-                        1.4166666666666668e-08,  # Pe = 100
-                        3.33e-9]  # Pe = 425.4
-    for i in dispersion_value:
-        column.axial_dispersion = i  
+       
+    fig, axes = plt.subplots(3,2, figsize = [10, 12]) 
+    axes = axes.flatten()
+ 
+    peclet_numbers = [1, 5, 25, 50, 100, 255]
+    for i, peclet_number in enumerate(peclet_numbers):
+        column.axial_dispersion = (column.length * (0.3 * (1e-2 / 60))) / peclet_number
         simulation_results = process_simulator.simulate(process)
-        simulation_results.solution.column.outlet.plot()
-        #CADETProcess.plotting.Layout.check_required_parameters()
-        # layout = {"title": str(i)}
-        #simulation_results.solution.column.outlet.plot.plotting.Layout.check_required_parameters()
+        simulation_results.solution.column.outlet.plot(ax = axes[i])
+        axes[i].set_title(f"Peclet number: {peclet_number}")
+
+    plt.tight_layout()
