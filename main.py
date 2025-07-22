df2bacca575aab0dab0601767a9c53e172987743
Hannah Lanzrath <h.lanzrath@fz-juelich.de>
2025-07-22 15:10:43 +0200

    Change Options

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from cadetrdm import process_example, Options
 if __name__ == "__main__":
     options = Options()
     options.commit_message = "Commit Message Test"
-    options.debug = True
-    options.push = True
+    options.debug = False
+    options.push = False
     options.source_directory = "src"
     options.branch_prefix = "Experiment_A"
     process_example(options)
