22b2a41d6c7eb91a8979492b0bcc42cacd84a8f9
Hannah Lanzrath <h.lanzrath@fz-juelich.de>
2025-07-22 14:53:37 +0200

    Add Branch prefix

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from cadetrdm import process_example, Options
 if __name__ == "__main__":
     options = Options()
     options.commit_message = "Commit Message Test"
-    options.debug = False
+    options.debug = True
     options.push = True
     options.source_directory = "src"
+    options.branch_prefix = "Experiment_A"
     process_example(options)
