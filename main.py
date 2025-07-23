6d45d5cf4353daeadd41ba2f94e47344149f7708
Katharina Paul <katharina.paul2403@gmail.com>
2025-07-23 10:10:07 +0200

    test run

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cadetrdm import process_example, Options
 
 if __name__ == "__main__":
     options = Options()
-    options.commit_message = "Commit Message Test"
+        options.commit_message = "Test run"
     options.debug = False
     options.push = True
     options.source_directory = "src"
