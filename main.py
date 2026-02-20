f1c3276291c678f338daf9b92ad9ae7fbfa3ea73
Hannah Lanzrath <h.lanzrath@fz-juelich.de>
2026-02-20 15:40:52 +0100

    Test with push=False

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     options = Options()
     options.commit_message = "Rectangular Pulse Example"
     options.debug = False
-    options.push = True
+    options.push = False
     options.source_directory = "src"
     process_example(options)
