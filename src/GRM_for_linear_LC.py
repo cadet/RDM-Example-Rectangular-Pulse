99c1a5fec0f83292c2cd177d884dfd65c3020898
Hannah Lanzrath <h.lanzrath@fz-juelich.de>
2026-03-25 15:02:37 +0100

    Update jupytex version to 1.19.1

--- src/GRM_for_linear_LC.py
+++ src/GRM_for_linear_LC.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.17.2
+#       jupytext_version: 1.19.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -15,7 +15,7 @@
 
 # %% [markdown]
 # (GRM_for_linear_LC)=
-# # Effects of the Peclet number on the elution of a rectangular pulse 
+# # Elution of a rectangular pulse
 
 # %% [markdown]
 # This case study is a reproduction of parts of the results published in:
